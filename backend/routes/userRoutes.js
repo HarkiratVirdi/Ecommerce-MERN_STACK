274c83a0f5e3f0c5e60945dcbeb93048a9a7f6c3
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-01-15 02:57:37 -0500

    update user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { loginUser, getUserProfile } from "../controllers/userController.js";
+import {
+  loginUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+} from "../controllers/userController.js";
 import protect from "../middleware/authMiddleware.js";
 const router = express.Router();
 
+router.route("/").post(registerUser);
 router.post("/login", loginUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 export default router;
