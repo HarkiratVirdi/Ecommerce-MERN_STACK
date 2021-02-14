8cd862a389bdb26a59ab51eaf30463f15e153047
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-02-14 13:28:03 -0500

    fixed order id

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -15,7 +15,6 @@ const addOrderItems = asyncHandler(async (req, res) => {
   if (orderItems && orderItems.length === 0) {
     res.status(400);
     throw new Error("No Order item");
-    return;
   } else {
     const order = new Order({
       orderItems,
@@ -29,7 +28,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
     });
 
     const createdOrder = await order.save();
-    res.status(201).json({ createdOrder });
+    res.status(201).json(createdOrder);
   }
 });
 
