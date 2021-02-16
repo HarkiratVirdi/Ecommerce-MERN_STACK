5105f987a3cbd39ece04196bc7e783a8c7aa784a
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-02-15 23:01:49 -0500

    order aciton done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -45,27 +45,25 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-const updateOrderToPaid = asyncHandler(async(req, res) => {
+const updateOrderToPaid = asyncHandler(async (req, res) => {
   const order = await Order.findById(req.params.id);
 
-  if(order)
-  {
+  if (order) {
     order.isPaid = true;
     order.paidAt = Date.now();
     order.paymentResult = {
       id: req.body.id,
       status: req.body.status,
       update_time: req.body.update_time,
-      email_address: req.body.payer.email_address
-    }
+      email_address: req.body.payer.email_address,
+    };
     const updatedOrder = await order.save();
 
     res.json(updatedOrder);
-    }else{
-      res.status(404);
-      throw new Error("Order not found");
-    }
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
   }
-})
+});
 
 export { addOrderItems, getOrderById, updateOrderToPaid };
